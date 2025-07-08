1ab84c7af4d79befc4c213de4cc09e6761bd1c55
IwantToBeProgrammer17 <azky.haekal35@smp.belajar.id>
2025-07-09 06:03:30 +0700

    class

--- js2.js
+++ js2.js
@@ -81,11 +81,43 @@ class Person {
   }
 
   sleep() {
-    //jika tidur, energy kembali ke 100
+    this.energy = 100;
+    console.log(`${this.name} tidur dan energinya kembali menjadi 100.`);
   }
-
+  
   eat() {
-    //jika makan , energy bertambah 10% dari jumlah makanan yang dimakan
+    if (arguments.length === 0) {
+      console.log(
+        "Tolong masukkan jumlah makanan yang dimakan sebagai parameter."
+      );
+      return;
+    }
+    const jumlah = arguments[0];
+    const tambahEnergi = jumlah * 0.1;
+    this.energy += tambahEnergi;
+    console.log(
+      `${this.name} makan sebanyak ${jumlah}, energinya bertambah ${tambahEnergi} menjadi ${this.energy}.`
+    );
+  }
+  run(length) {
+    console.log(`${this.name} runs for ${length} kms.`);
+    this.energy = this.energy - length * 0.5;
+    console.log(`${this.name}'s energy is now ${this.energy}.`);
+  }
+  swim(length) {
+    console.log(`${this.name} swims for ${length} kms.`);
+    this.energy = this.energy - length * 0.8;
+    console.log(`${this.name}'s energy is now ${this.energy}.`);
+  }
+  talk(minutes) {
+    console.log(`${this.name} talks for ${minutes} minutes.`);
+    this.energy = this.energy - minutes * 0.1;
+    console.log(`${this.name}'s energy is now ${this.energy}.`);
+  }
+  play(minutes) {
+    console.log(`${this.name} plays for ${minutes} minutes.`);
+    this.energy = this.energy - minutes * 0.2;
+    console.log(`${this.name}'s energy now ${this.energy}.`);
   }
 }
 
@@ -94,5 +126,4 @@ console.log(person1);
 person1.getage();
 console.log(performance.energy);
 person1.walk(10);
-
-
+person1.eat(10);
